a7d82e8939a402837bfc0fc3939d24fb65f02858
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-10-08 15:45:49 +0530

    - Project creation and Initial commit

---
+++ montra/montraApp.swift
@@ -0,0 +1,17 @@
+//
+//  montraApp.swift
+//  montra
+//
+//  Created by Rushikesh Pandit on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct montraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
