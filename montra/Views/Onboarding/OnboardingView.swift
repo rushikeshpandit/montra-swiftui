4c4a608db5cc5793cf4eee17f69b765a2eb94d6e
Rushikesh Pandit <13253098+rushikeshpandit@users.noreply.github.com>
2023-10-16 12:51:24 +0530

    Ma 3 introductions screens (#1)
    * - Added onboarding states
    
    * - Added file for Introduction page
    
    * - Splashscreen code refactor
    
    * - Making splash screen fullscreen
    
    * - pbxproj changes
    
    * - Added new color for subtitles on onboarding page
    
    * - Refactored code to render image on onboarding page
    
    * - Added title text on onboarding page
    
    * - Added subtitle text on onboarding page
    
    * - Code refactor
    
    * - Added horizontal padding for onboarding page
    
    * - Added new color
    
    * - Added page indicator
    
    * - Added swiftlint
    
    * - Code refactor
    
    * - Added page indicator
    
    * - Updated onboarding view code
    
    * - pbxproj changes

---
+++ montra/Views/Onboarding/OnboardingView.swift
@@ -0,0 +1,119 @@
+//
+//  OnboardingView.swift
+//  montra
+//
+//  Created by Rushikesh Pandit on 08/10/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    @State var currentOnboardingState: OnboardingStates = .one
+    @State var currentIndex: Int = 0
+    @State var title: String = ""
+    @State var subTitle: String = ""
+    let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+
+    @ViewBuilder func getImage() -> some View {
+        switch currentOnboardingState {
+            case .one:
+                Image("intro_page_one")
+            case .two:
+                Image("intro_page_two")
+            case .three:
+                Image("intro_page_three")
+        }
+    }
+
+    @ViewBuilder func getTitleText() -> some View {
+        switch currentOnboardingState {
+            case .one:
+                Text("Gain total control of your money")
+            case .two:
+                Text("Know where your money goes")
+            case .three:
+                Text("Planning ahead\n")
+        }
+    }
+
+    @ViewBuilder func getSubTitleText() -> some View {
+        switch currentOnboardingState {
+            case .one:
+                Text("Become your own money manager and make every cent count")
+            case .two:
+                Text("Track your transactions easily, with categories and financial report")
+            case .three:
+                Text("Setup your budget for each category so you are in control")
+        }
+    }
+
+    var body: some View {
+        VStack {
+            getImage()
+                .padding(.vertical, 20)
+            getTitleText()
+                .padding(.vertical, 20)
+                .font(.custom("Inter-Bold", fixedSize: 32))
+                .frame(alignment: .center)
+                .multilineTextAlignment(.center)
+            getSubTitleText()
+                .padding(.vertical, 20)
+                .font(.custom("Inter-Medium", fixedSize: 16))
+                .frame(alignment: .center)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.subTitleText)
+            PageIndicator(numPages: 3, currentPage: $currentIndex)
+                .padding(.vertical, 20)
+            Button(action: {
+                print("Sign Up")
+            }) {
+                Text("Sign Up")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .background(Color.launchScreenBackground)
+                    .cornerRadius(16)
+            }
+            .padding(.top, 40)
+            .padding(.bottom, 20)
+            Button(action: {
+                print("Login")
+            }) {
+                Text("Login")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(Color.launchScreenBackground)
+                    .background(Color.primaryLight)
+                    .cornerRadius(16)
+            }
+        }
+        .onReceive(timer) { _ in
+            if currentIndex != 2 {
+                currentIndex +=  1
+            } else {
+                currentIndex = 0
+            }
+            switch currentOnboardingState {
+                case .one:
+                    currentOnboardingState = .two
+                    break
+                case .two:
+                    currentOnboardingState = .three
+                    break
+                case .three:
+                    currentOnboardingState = .one
+                    break
+            }
+        }
+        .padding(.horizontal, 60)
+        .frame(maxWidth: .infinity,
+               maxHeight: .infinity,
+               alignment: .center)
+        .background(Color.white)
+
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
