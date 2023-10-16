59e7075b92d88a1a52202d38d9d921f74d18cd01
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-10-16 18:35:51 +0530

    - Added view for signup

---
+++ montra/Views/Onboarding/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  montra
+//
+//  Created by Rushikesh Pandit on 16/10/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
